58f515b6bfbe7016d1000cdb4c53ace84e84086e
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-07 13:57:22 -0500

    added explicit else to #apply_discount

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,8 +24,9 @@ class CashRegister
     if @discount
       @total -= (total * discount/100.0)
       return "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      return "There is no discount to apply."
     end
-    return "There is no discount to apply."
   end
 
   def items 
